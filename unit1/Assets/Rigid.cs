f3e816e037163b388283624e229dbfea5facbf2c
Braden Toeppe <27ToepB@elida.k12.oh.us>
2024-09-09 10:52:14 -0400

    camera fallow player

--- unit1/Assets/Rigid.cs
+++ unit1/Assets/Rigid.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Rigid : MonoBehaviour
 {
-   public float speed = 20.0f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
     }
 }
